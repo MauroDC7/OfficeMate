70d185e2f592760c2bb729cf1a46503013b22fa4
MauroDC7 <132913156+MauroDC7@users.noreply.github.com>
2026-05-11 22:05:32 +0200

    updated and added new models

---
+++ app/Models/DesktopActivity.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+#[Fillable([
+    'user_id',
+    'app_name',
+    'window_title',
+    'browser_url',
+    'browser_domain',
+    'browser_tab_title',
+    'started_at',
+    'ended_at',
+    'duration_seconds',
+])]
+class DesktopActivity extends Model
+{
+    /**
+     * @return BelongsTo<User, $this>
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'started_at' => 'datetime',
+            'ended_at' => 'datetime',
+        ];
+    }
+}
